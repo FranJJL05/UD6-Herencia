9adb134d801c18c83cd6453bd2ca63ecd8a24358
FranJJL05 <fjimlop182@g.educaand.es>
2025-03-13 13:07:42 +0100

    ejemplo uso de arraylist

---
+++ Ejercicio clase/Ejemplo1.java
@@ -0,0 +1,18 @@
+
+public class Ejemplo1{
+
+ArrayList<String> nombres = new ArrayList<>();
+nombres.add("Ana");
+nombres.add("Luisa");
+nombres.add("Felipe");
+System.out.println(nombres); // [Ana, Luisa, Felipe]
+nombres.add(1, "Pablo");
+System.out.println(nombres); // [Ana, Pablo, Luisa, Felipe]                                                       
+nombres.remove(0);
+System.out.println(nombres); // [Pablo, Luisa, Felipe]
+nombres.set(0,"Alfonso");
+System.out.println(nombres); // [Alfonso, Luisa, Felipe]                                                          
+String s = nombres.get(1);
+String ultimo = nombres.get(nombres.size() - 1);
+System.out.println(s + " " + ultimo);  // Luisa Felipe
+}
